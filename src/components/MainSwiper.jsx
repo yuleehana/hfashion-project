2ff4095ff63cce594f24bdd578f7b8765e066e8e
은희 <withjesus211@gmail.com>
2025-11-12 15:19:19 +0900

    KIM:메인슬라이더 스타일 추가

--- src/components/MainSwiper.jsx
+++ src/components/MainSwiper.jsx
@@ -4,33 +4,72 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './sass/MainSwiper.scss'
 
 const MainSwiper = () => {
     // 메인이미지 저장 배열
     const slides = [
-        { src: "./images/main-banner1.jpg", alt: "slider1" },
-        { src: "./images/main-banner2.jpg", alt: "slider2" },
-        { src: "./images/main-banner3.jpg", alt: "slider3" },
-        { src: "./images/main-banner4.jpg", alt: "slider4" },
-        { src: "./images/main-banner5.jpg", alt: "slider5" },
-        { src: "./images/main-banner6.jpg", alt: "slider6" },
-        { src: "./images/main-banner7.jpg", alt: "slider7" },
-        { src: "./images/main-banner8.jpg", alt: "slider8" },
-        { src: "./images/main-banner9.jpg", alt: "slider9" },
+        {
+            src: "./images/main-banner1.jpg", alt: "slider1",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner2.jpg", alt: "slider2",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner3.jpg", alt: "slider3",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner4.jpg", alt: "slider4",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner5.jpg", alt: "slider5",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner6.jpg", alt: "slider6",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner7.jpg", alt: "slider7",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner8.jpg", alt: "slider8",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
+        {
+            src: "./images/main-banner9.jpg", alt: "slider9",
+            desc: ['TOMMY HILFIGER', '힐피거 레이싱 클럽', '스타들과 함께한 가을 컬렉션 런칭 행사']
+        },
     ]
 
 
     return (
         <div className="main-sliders">
             <Swiper
-                modules={[Navigation, Autoplay, Pagination]}
+                modules={[Autoplay, Navigation, Pagination]}
                 autoplay={{ delay: 3000 }}
-                pagination
                 navigation
-                loop={true}>
+                pagination
+                loop={true}
+                centeredSlides={true}
+                slidesPerView={1.8}
+                spaceBetween={20}
+            >
                 {slides.map((slide, id) => (
                     <SwiperSlide key={id}>
-                        <img src={slide.src} alt={slide.alt} />
+                        <div className="img-box">
+                            <img src={slide.src} alt={slide.alt} />
+                        </div>
+                        <div className="text-box">
+                            <p>{slide.desc[0]}</p>
+                            <p>{slide.desc[1]}</p>
+                            <p>{slide.desc[2]}</p>
+                        </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
