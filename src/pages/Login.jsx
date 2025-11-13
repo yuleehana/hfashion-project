2ff66a0fcae9bee843c8b6fafee3550a2bb7f710
ukum <97ukum@gmail.com>
2025-11-13 10:57:58 +0900

    1113 main_merge

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -50,36 +50,6 @@ const Login = () => {
             <button>회원</button>
             <button onClick={handleNonMember}>비회원</button>
           </div>
-
-          {/* <form onSubmit={handleSubmit}>
-                        <input type='email'
-                            value={email} placeholder='아이디 또는 이메일'
-                            required
-                            onChange={(e) => setEmail(e.target.value)} />
-                        <input type='password'
-                            value={password} placeholder='비밀번호 입력 (8~15자리 영문+숫자+특수문자 조합)'
-                            required
-                            onChange={(e) => setPassword(e.target.value)} />
-
-                        <div className='login-op'>
-                            <div className='id-save'>
-                                <img src='./images/check-icon.svg' alt='체크아이콘' />
-                                <p>아이디 저장</p>
-                            </div>
-                            <button>아이디찾기 | 비밀번호찾기</button>
-                        </div>
-
-                        <div className='login-btn-wrap'>
-                            <div className='login-btn-wrap-top'>
-                                <button type='submit'>로그인하기</button>
-                            </div>
-                            <div className='login-btn-wrap-bottom'>
-                                <button className='google' onClick={handleGoogleLogin}>Google</button>
-                                <button>H point 통합회원</button>
-                            </div>
-
-                        </div>
-                    </form> */}
           {isLogin ? <MemberLogin /> : <NoMemberLogin />}
           <div>
             <Link to="/join">지금 회원가입하러 가기</Link>
