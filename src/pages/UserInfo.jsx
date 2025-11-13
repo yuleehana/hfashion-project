92d018650335f38545bacc9b5c80984dbe130cbf
Oh Yeongseo <wnrdmadmlane@naver.com>
2025-11-13 12:40:19 +0900

    오영서 UserInfo.jsx 칸 정리

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -26,50 +26,53 @@ const UserInfo = () => {
     return (
         <div className='sub-page'>
             <div className='content-inner'>
-                <div className='user-info-left'>
-                    <UserInfoLeftMenu />
-                </div>
+                <div className="content-top">
+                    <div className='user-info-left'>
+                        <UserInfoLeftMenu />
+                    </div>
 
-                <div className='user-info-right'>
-                    {/* <h2>{user.name}</h2>
-                    <p>{user.email}</p>
-                    <button onClick={handleLogout}>로그아웃</button> */}
-                    <div className="user-idname-box">
-                        <div className="profile-box">
-                            <img src="./images/my-icon-black.svg" alt="profile" />
-                        </div>
-                        <div className="profile-item-box">
-                            <div className="item-box">
-                                <div className="user-text">
-                                    <p>{`<span>${user.email}(${user.name})</span>님`}</p>
-                                    <p>환영합니다</p>
+                    <div className='user-info-right'>
+                        {/* <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                        <button onClick={handleLogout}>로그아웃</button> */}
+                        <div className="user-idname-box">
+                            <div className="profile-box">
+                                <img src="./images/my-icon-black.svg" alt="profile" />
+                            </div>
+                            <div className="profile-item-box">
+                                <div className="item-box">
+                                    <div className="user-text">
+                                        <p>{`<span>${user.email}(${user.name})</span>님`}</p>
+                                        <p>환영합니다</p>
+                                    </div>
+                                </div>
+
+                                <div className="btn-box">
+                                    <button>로그아웃</button>
+                                    <button>내 회원정보</button>
                                 </div>
                             </div>
-
-                            <div className="btn-box">
-                                <button>로그아웃</button>
-                                <button>내 회원정보</button>
-                            </div>
                         </div>
-                    </div>
 
-                    <div className="point-card-box">
-                        <div className="user-point-box">
-                            <p>내 포인트</p>
-                            <div className="point-price">
-                                <span>0</span>
-                                <span>P</span>
+                        <div className="point-card-box">
+                            <div className="user-point-box">
+                                <p>내 포인트</p>
+                                <div className="point-price">
+                                    <span>0</span>
+                                    <span>P</span>
+                                </div>
                             </div>
-                        </div>
-                        <div className="point-favor-list">
-                            <p>내 포인트 혜택</p>
-                            <div className="favor-list">
-                                <div className="earn-favor">최대 5% 적립 가능</div>
-                                <div className="delivery-free">N배송 무료</div>
+                            <div className="point-favor-list">
+                                <p>내 포인트 혜택</p>
+                                <div className="favor-list">
+                                    <div className="earn-favor">최대 5% 적립 가능</div>
+                                    <div className="delivery-free">N배송 무료</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                
             </div>
         </div>
     )
