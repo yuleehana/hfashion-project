a3af145bb77bd633b005b3286ff10e0b27f9b724
Oh Yeongseo <wnrdmadmlane@naver.com>
2025-11-14 09:40:01 +0900

    오영서 메인 베스트리뷰 작업

--- src/components/MainBestReview.jsx
+++ src/components/MainBestReview.jsx
@@ -6,61 +6,66 @@ const MainBestReview = () => {
         <section>
             <h2>BEST REVIEW</h2>
             <div className='container'>
-                <div className='review-box'>
-                    <div className="img-box">
-                        <img src="./images/products/POYSU2550534378001/thumbnail.jpg" alt="" />
+                <div className="review-box-list">
+                    <div className='review-box'>
+                        <div className="img-box">
+                            <img src="./images/products/POYSU2550534378001/thumbnail.jpg" alt="" />
+                        </div>
+
+                        <div className="review-item-box">
+                            <div className="item-top">
+                                <p className="brand-name">BOSS</p>
+                                <p className='review-text'>리뷰글리뷰글리뷰글</p>
+                            </div>
+                            <div className="item-bottom">
+                                <p className="product-name">스트레치 소프트 치노 스트레이트핏</p>
+                                <div className="rating-box">
+                                    <span>평점</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="review-item-box">
-                        <div className="item-top">
-                            <p className="brand-name">BOSS</p>
-                            <p className='review-text'>리뷰글리뷰글리뷰글</p>
+                    <div className='review-box'>
+                        <div className="img-box">
+                            <img src="./images/products/POYSU2550534378001/thumbnail.jpg" alt="" />
                         </div>
-                        <div className="item-bottom">
-                            <p className="product-name">스트레치 소프트 치노 스트레이트핏</p>
-                            <div className="rating-box">
-                                <span>평점</span>
+
+                        <div className="review-item-box">
+                            <div className="item-top">
+                                <p className="brand-name">BOSS</p>
+                                <p className='review-text'>리뷰글리뷰글리뷰글</p>
+                            </div>
+                            <div className="item-bottom">
+                                <p className="product-name">스트레치 소프트 치노 스트레이트핏</p>
+                                <div className="rating-box">
+                                    <span>평점</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='review-box'>
+                        <div className="img-box">
+                            <img src="./images/products/POYSU2550534378001/thumbnail.jpg" alt="" />
+                        </div>
+
+                        <div className="review-item-box">
+                            <div className="item-top">
+                                <p className="brand-name">BOSS</p>
+                                <p className='review-text'>리뷰글리뷰글리뷰글</p>
+                            </div>
+                            <div className="item-bottom">
+                                <p className="product-name">스트레치 소프트 치노 스트레이트핏</p>
+                                <div className="rating-box">
+                                    <span>평점</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-                <div className='review-box'>
-                    <div className="img-box">
-                        <img src="./images/products/POYSU2550534378001/thumbnail.jpg" alt="" />
-                    </div>
-
-                    <div className="review-item-box">
-                        <div className="item-top">
-                            <p className="brand-name">BOSS</p>
-                            <p className='review-text'>리뷰글리뷰글리뷰글</p>
-                        </div>
-                        <div className="item-bottom">
-                            <p className="product-name">스트레치 소프트 치노 스트레이트핏</p>
-                            <div className="rating-box">
-                                <span>평점</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='review-box'>
-                    <div className="img-box">
-                        <img src="./images/products/POYSU2550534378001/thumbnail.jpg" alt="" />
-                    </div>
-
-                    <div className="review-item-box">
-                        <div className="item-top">
-                            <p className="brand-name">BOSS</p>
-                            <p className='review-text'>리뷰글리뷰글리뷰글</p>
-                        </div>
-                        <div className="item-bottom">
-                            <p className="product-name">스트레치 소프트 치노 스트레이트핏</p>
-                            <div className="rating-box">
-                                <span>평점</span>
-                            </div>
-                        </div>
-                    </div>
+                <div className="more-btn-wrap">
+                    <button className='more-btn'>리뷰 더보기</button>
                 </div>
             </div>
         </section>
