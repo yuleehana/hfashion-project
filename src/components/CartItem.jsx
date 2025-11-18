7e63f7f896f992e17662eafd03f739813908e073
yuleehana <yuleehana@gmail.com>
2025-11-18 16:03:02 +0900

    카트페이지 사스 추가_main_1118

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -11,40 +11,45 @@ const CartItem = () => {
     <div className='cart-item-list-wrap'>
       <ul className='cart-item-list'>
         {cartItems.map((item, id) => (
-          <li key={id} className='cart-item'>
+          <>
+            <li key={id} className='cart-item'>
 
-            <div className='cart-item-inner'>
+              <div className='cart-item-inner'>
 
-              <div className='item-left'>
-                <span className='checkbox'><img src="/" alt=" " /></span>
-                <div className='item-img-box'>
-                  <img src={item.thumbImg} alt={item.code} />
-                </div>
-                <div className='item-text-box'>
-                  <div className='item-desc'>
-                    <Link><span className='item-brand'>{item.brand}</span></Link>
-                    <span className='item-title'>{item.title}</span>
-                    <span className='item-op'>{item.color}/{item.size}</span>
+                <div className='item-left'>
+                  <Link className='checkbox'>
+                    <img src="../public/images/check-icon.svg" alt="선택" className='check' />
+                  </Link>
+                  <div className='item-img-box'>
+                    <img src={item.thumbImg} alt={item.code} />
                   </div>
-                  <div className='op-change'>
-                    <span>옵션변경</span>
+                  <div className='item-text-box'>
+                    <div className='item-desc'>
+                      <Link><span className='item-brand'>{item.brand}</span></Link>
+                      <span className='item-title'>{item.title}</span>
+                      <span className='item-op'>색상 : {item.color}/사이즈 : {item.size}</span>
+                    </div>
+                    <div className='op-change'>
+                      <span>옵션변경</span>
+                    </div>
                   </div>
                 </div>
+
+                <div className='item-right'>
+                  <span>수량</span>
+                  <span>원</span>
+                  <button>바로구매</button>
+                </div>
+
+                <span className='del-icon' onClick={() => onRemoveCart(item.code, item.size)}>
+                  <img src="../public/images/close-icon-black.svg" alt="아이템 삭제" />
+                </span>
+
               </div>
 
-              <div className='item-right'>
-                <span>수량</span>
-                <span>원</span>
-                <button>바로구매</button>
-              </div>
-
-              <span className='del-icon' onClick={() => onRemoveCart(item.code, item.size)}>
-                <img src="../public/images/close-icon-black.svg" alt="아이템 삭제" />
-              </span>
-
-            </div>
-
-          </li>
+            </li>
+            <hr></hr>
+          </>
         ))}
       </ul>
     </div>
