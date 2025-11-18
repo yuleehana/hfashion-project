5ea6d5c120e0521f1da99876ef9fb504caa5c84b
millllllllllllll <for12121531@gmail.com>
2025-11-18 18:21:29 +0900

    search_1118

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react'; // 검색창 오픈 상태용 useState 추가 11/18
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authstore';
 import './sass/Header.scss';
 import { usePickStore } from '../store/usePickStore';
+import SearchOverlay from './SearchOverlay'; // 오버레이 검색창 컴포넌트 11/18
 
 // mainmenu
 const menus = [
@@ -67,6 +68,9 @@ const Header = () => {
   const location = useLocation();
   const currentPath = location.pathname;
 
+    // 검색창 열림 상태 관리 11/18
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   // 메서드
   const handleLogout = () => {
     resetPcikList();
@@ -94,6 +98,7 @@ const Header = () => {
   };
 
   return (
+     <>
     <header>
       <div className="header-inner">
         <div className="inner-left">
@@ -129,9 +134,13 @@ const Header = () => {
         <div className="inner-right">
           <ul className="gnb-list">
             <li>
-              <Link to="/search">
+              {/* 검색 아이콘 클릭 시 오버레이 열기 11/18*/}
+                <button onClick={() => setIsSearchOpen(true)} className="search-button">
+                  <img src="/images/search-icon-white.svg" alt="검색아이콘" />
+                </button>
+              {/* <Link to="/search">
                 <img src="/images/search-icon-white.svg" alt="검색아이콘" />
-              </Link>
+              </Link> */}
             </li>
             {user ? (
               <>
@@ -164,6 +173,12 @@ const Header = () => {
         </div>
       </div>
     </header>
+
+          {/*  검색 오버레이 컴포넌트 렌더링 */}
+      <SearchOverlay
+        isOpen={isSearchOpen}
+        onClose={() => setIsSearchOpen(false)}/>
+         </>
   );
 };
 
