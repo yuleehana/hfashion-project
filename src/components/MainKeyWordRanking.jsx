7b9f489be90698914e73cd9f6bcb3e99d3be6417
Oh Yeongseo <wnrdmadmlane@naver.com>
2025-11-18 16:00:50 +0900

    오영서 MainKeyWordRanking.jsx 작업

--- src/components/MainKeyWordRanking.jsx
+++ src/components/MainKeyWordRanking.jsx
@@ -3,144 +3,149 @@ import RankingItem from './RankingItem';
 import { Link, useNavigate } from 'react-router-dom';
 import "./sass/MainKeyWordRanking.scss";
 
-const rightMainData = [
-  {
-    id: 1,
-    img: '/images/products/T32F7WSH13TWT1MAS/thumbnail.jpg',
-    brand: 'TOMMY JEANS',
-    originprice: 139000,
-    dis: '15%',
-  },
-];
+// const rightMainData = [
+//   {
+//     id: 1,
+//     img: '/images/products/T32F7WSH13TWT1MAS/thumbnail.jpg',
+//     brand: 'TOMMY JEANS',
+//     originprice: 139000,
+//     dis: '15%',
+//   },
+// ];
 
-const rightSubData = [
-  [
-    {
-      id: 1,
-      img: '/images/products/T32F6WSH02TWT10P4/thumbnail.jpg',
-      brand: 'TOMMY JEANS',
-      title: '마이크로 깅엄 체크 셔츠',
-      originprice: 129000,
-      dis: '15%',
-    },
-    {
-      id: 2,
-      img: '/images/products/T32F6WSH10TWL11BZ/thumbnail.jpg',
-      brand: 'TOMMY JEANS',
-      title: '데님 크롭 셔켓',
-      originprice: 169000,
-      dis: '15%',
-    },
-    {
-      id: 3,
-      img: '/images/products/T32F7NEF41TWT11AB/thumbnail.jpg',
-      brand: 'TOMMY JEANS',
-      title: '벨라 플레어 핏 데님 팬츠',
-      originprice: 239000,
-      dis: '15%',
-    },
-  ],
-];
+// const rightSubData = [
+//   [
+//     {
+//       id: 1,
+//       img: '/images/products/T32F6WSH02TWT10P4/thumbnail.jpg',
+//       brand: 'TOMMY JEANS',
+//       title: '마이크로 깅엄 체크 셔츠',
+//       originprice: 129000,
+//       dis: '15%',
+//     },
+//     {
+//       id: 2,
+//       img: '/images/products/T32F6WSH10TWL11BZ/thumbnail.jpg',
+//       brand: 'TOMMY JEANS',
+//       title: '데님 크롭 셔켓',
+//       originprice: 169000,
+//       dis: '15%',
+//     },
+//     {
+//       id: 3,
+//       img: '/images/products/T32F7NEF41TWT11AB/thumbnail.jpg',
+//       brand: 'TOMMY JEANS',
+//       title: '벨라 플레어 핏 데님 팬츠',
+//       originprice: 239000,
+//       dis: '15%',
+//     },
+//   ],
+// ];
 
 const MainKeyWordRanking = () => {
   const [liMenu, setLiMenu] = useState(null);
   const navigate = useNavigate();
 
   const menuRankProduct = [
-    {title:"아우터", rank:[
-      {
-        id: 1,
-        img: '/images/products/POYFA2550543492001/thumbnail.jpg',
-        brand: 'BOSS',
-      }, {
-        id: 2,
-        img: '/images/products/POYFA2550543240202/thumbnail.jpg',
-        brand: 'BOSS',
-      }, {
-        id: 3,
-        img: '/images/products/POYFA2550543388404/thumbnail.jpg',
-        brand: 'BOSS',
-      }, {
-        id: 4,
-        img: '/images/products/POYSP2550530270001/thumbnail.jpg',
-        brand: 'BOSS',
-      }
+    {title:"아우터", mainImg:"/images/products/POYWI2450499551001/thumbnail.jpg",
+      rank:[
+        {
+          id: 1,
+          img: '/images/products/POYFA2550543492001/thumbnail.jpg',
+          brand: 'BOSS',
+        }, {
+          id: 2,
+          img: '/images/products/POYFA2550543240202/thumbnail.jpg',
+          brand: 'BOSS',
+        }, {
+          id: 3,
+          img: '/images/products/POYFA2550543388404/thumbnail.jpg',
+          brand: 'BOSS',
+        }, {
+          id: 4,
+          img: '/images/products/POYSP2550530270001/thumbnail.jpg',
+          brand: 'BOSS',
+        }
     ]},
-    {title:"펜츠", rank:[
-      {
-        id: 1,
-        img: "/images/products/T32F7NEW45TWT11BK/thumbnail.jpg",
-        brand: "TOMMY JEANS"
-      }, {
-        id: 2,
-        img: "/images/products/T12F6NPC010MT11BC/thumbnail.jpg",
-        brand: "TOMMY HILFIGER MEN"
-      }, {
-        id: 3,
-        img: "/images/products/T32F7WPC16TWT1C1G/thumbnail.jpg",
-        brand: "TOMMY JEANS"
-      }, {
-        id: 4,
-        img: '/images/products/T22F7WPC080WT1ADZ/thumbnail.jpg',
-        brand: 'TOMMY HILFIGER WOMEN'
-      }
+    {title:"펜츠", mainImg:"/images/products/T32F6NEW19TWT11BY/thumbnail.jpg",
+      rank:[
+        {
+          id: 1,
+          img: "/images/products/T32F7NEW45TWT11BK/thumbnail.jpg",
+          brand: "TOMMY JEANS"
+        }, {
+          id: 2,
+          img: "/images/products/T12F6NPC010MT11BC/thumbnail.jpg",
+          brand: "TOMMY HILFIGER MEN"
+        }, {
+          id: 3,
+          img: "/images/products/T32F7WPC16TWT1C1G/thumbnail.jpg",
+          brand: "TOMMY JEANS"
+        }, {
+          id: 4,
+          img: '/images/products/T22F7WPC080WT1ADZ/thumbnail.jpg',
+          brand: 'TOMMY HILFIGER WOMEN'
+        }
     ]},
-    {title:"가방", rank:[
-      {
-        id: 1,
-        img: '/images/products/P4W4D3116GUB1/thumbnail.jpg',
-        brand: 'Calvin Klein Accessories',
-      }, {
-        id: 2,
-        img: '/images/products/P4W4D3155GUB1/thumbnail.jpg',
-        brand: 'Calvin Klein Accessories',
-      }, {
-        id: 3,
-        img: '/images/products/POYSP2550535706001/thumbnail.jpg',
-        brand: 'HUGO',
-      }, {
-        id: 4,
-        img: '/images/products/Q7H0401DC/thumbnail.jpg',
-        brand: 'Magforce',
-      }
+    {title:"가방", mainImg:"/images/products/P4W4D3202GUB1/thumbnail.jpg",
+      rank:[
+        {
+          id: 1,
+          img: '/images/products/P4W4D3116GUB1/thumbnail.jpg',
+          brand: 'Calvin Klein Accessories',
+        }, {
+          id: 2,
+          img: '/images/products/P4W4D3155GUB1/thumbnail.jpg',
+          brand: 'Calvin Klein Accessories',
+        }, {
+          id: 3,
+          img: '/images/products/POYSP2550535706001/thumbnail.jpg',
+          brand: 'HUGO',
+        }, {
+          id: 4,
+          img: '/images/products/Q7H0401DC/thumbnail.jpg',
+          brand: 'Magforce',
+        }
     ]},
-    {title:"신발", rank:[
-      {
-        id: 1,
-        img: '/images/products/POYFA2450522833005/thumbnail.jpg',
-        brand: 'BOSS',
-      }, {
-        id: 2,
-        img: '/images/products/POYSP2550536504001/thumbnail.jpg',
-        brand: 'BOSS',
-      }, {
-        id: 3,
-        img: '/images/products/T52F7ARS130JT1YBI/thumbnail.jpg',
-        brand: 'TOMMY SHOES',
-      }, {
-        id: 4,
-        img: '/images/products/T52F7ARS130JT1YBI/thumbnail.jpg',
-        brand: 'TOMMY SHOES',
-      }
+    {title:"신발", mainImg:"/images/products/P4WYM0134401W/thumbnail.jpg",
+      rank:[
+        {
+          id: 1,
+          img: '/images/products/POYFA2450522833005/thumbnail.jpg',
+          brand: 'BOSS',
+        }, {
+          id: 2,
+          img: '/images/products/POYSP2550536504001/thumbnail.jpg',
+          brand: 'BOSS',
+        }, {
+          id: 3,
+          img: '/images/products/T52F7ARS130JT1YBI/thumbnail.jpg',
+          brand: 'TOMMY SHOES',
+        }, {
+          id: 4,
+          img: '/images/products/T52F7ARS130JT1YBI/thumbnail.jpg',
+          brand: 'TOMMY SHOES',
+        }
     ]},
-    {title:"스커트", rank:[
-      {
-        id: 1,
-        img: '/images/products/T22F7WSC505WL1N56/thumbnail.jpg',
-        brand: 'TOMMY HILFIGER WOMEN',
-      }, {
-        id: 2,
-        img: '/images/products/T22F7WSC080WT1MR8/thumbnail.jpg',
-        brand: 'TOMMY HILFIGER WOMEN',
-      }, {
-        id: 3,
-        img: '/images/products/T22F7WSC060WT1BDS/thumbnail.jpg',
-        brand: 'TOMMY HILFIGER WOMEN',
-      }, {
-        id: 4,
-        img: '/images/products/T22F7WSC061WT10OG/thumbnail.jpg',
-        brand: 'TOMMY HILFIGER WOMEN',
-      }
+    {title:"스커트", mainImg:"/images/products/T22F7WSC050WT1C1G/thumbnail.jpg",
+      rank:[
+        {
+          id: 1,
+          img: '/images/products/T22F7WSC505WL1N56/thumbnail.jpg',
+          brand: 'TOMMY HILFIGER WOMEN',
+        }, {
+          id: 2,
+          img: '/images/products/T22F7WSC080WT1MR8/thumbnail.jpg',
+          brand: 'TOMMY HILFIGER WOMEN',
+        }, {
+          id: 3,
+          img: '/images/products/T22F7WSC060WT1BDS/thumbnail.jpg',
+          brand: 'TOMMY HILFIGER WOMEN',
+        }, {
+          id: 4,
+          img: '/images/products/T22F7WSC061WT10OG/thumbnail.jpg',
+          brand: 'TOMMY HILFIGER WOMEN',
+        }
     ]}
   ]
 
