a2dee4c7bfd245390ff3b4399cf07e28e2aeeee7
은희 <withjesus211@gmail.com>
2025-11-20 12:17:01 +0900

    KIM:user, cart 스타일 추가

--- src/components/CartPopup.jsx
+++ src/components/CartPopup.jsx
@@ -7,17 +7,17 @@ const CartPopup = ({ onClose }) => {
     <div className='cart-popup-wrap' onClick={onClose}>
       <div className='cart-popup' onClick={(e) => e.stopPropagation()}>
         <div className='cart-popup-top'>
-          <h2>장바구니 담기 완료</h2>
+          <h4>장바구니 담기 완료</h4>
         </div>
 
         <div className='cart-popup-middle'>
-          <span>해당 상품이 장바구니에 담겼습니다.</span>
-          <span>장바구니로 이동하시겠습니까?</span>
+          <p>해당 상품이 장바구니에 담겼습니다.</p>
+          <p>장바구니로 이동하시겠습니까?</p>
         </div>
 
         <div className='cart-popup-bottom'>
-          <button onClick={onClose}>계속 쇼핑하기</button>
-          <Link to='/cart'><button>장바구니 보기</button></Link>
+          <button className='btn middle secondary' onClick={onClose}>계속 쇼핑하기</button>
+          <Link to='/cart'><button className='btn middle primary'>장바구니 보기</button></Link>
         </div>
       </div>
     </div>
