087b6dd17be769a3979f4d09ce35ee39bc16fc35
ukum <97ukum@gmail.com>
2025-11-20 15:44:46 +0900

    1120 kyk

--- src/pages/BrandDefault.jsx
+++ src/pages/BrandDefault.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import BrandListPage from './BrandListPage'
-import './sass/SubMain.scss'
+import React, { useState } from 'react';
+import BrandListPage from './BrandListPage';
+import './sass/SubMain.scss';
 import SubMainBestSeller from '../components/SubMainBestSeller';
-
+import { useProductStore } from '../store/useProductStore';
 
 const TargetBrands = [
-    'ROUGELOUNGE',
-    'TOMMY HILFIGER WOMEN',
-    'TOMMY HILFIGER MEN',
-    'TOMMY Shoes',
-    'TOMMY JEANS',
-    'SJYP',
+  'ROUGELOUNGE',
+  'TOMMY HILFIGER WOMEN',
+  'TOMMY HILFIGER MEN',
+  'TOMMY Shoes',
+  'TOMMY JEANS',
+  'SJYP',
 ];
 
 const BrandDefault = () => {
-    return (
-        <div className='sub-main'>
-            <h2 className='sub-main-title'>BRAND’S SHOP</h2>
-            <h3>BEST SELLER</h3>
-            <SubMainBestSeller brand={TargetBrands} />
-            <h3>BRAND</h3>
-            <BrandListPage brand={TargetBrands} />
-        </div>
-    )
-}
+  // ------------------------------------------------------------------------------
 
-export default BrandDefault
+  return (
+    <div className="sub-main">
+      <h2 className="sub-main-title">BRAND’S SHOP</h2>
+      <h3>BEST SELLER</h3>
+      <SubMainBestSeller brand={TargetBrands} />
+      <h3>BRAND</h3>
+      <BrandListPage brand={TargetBrands} />
+    </div>
+  );
+};
 
+export default BrandDefault;
