6e0b3e718fdd6ff3214ccf82c61f753f96cfbba2
yuleehana <yuleehana@gmail.com>
2025-11-29 15:02:18 +0900

    test9

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -29,6 +29,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
