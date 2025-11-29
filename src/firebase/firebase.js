fe93ff9e1564a6aeaa8a7781e5e48568c068372d
yuleehana <yuleehana@gmail.com>
2025-11-29 13:05:20 +0900

    test7

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,19 +4,19 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEN_SENDER_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEN_SENDER_ID,
 
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  apiKey: "AIzaSyDD6lh6FVHDXQE4K9ZvFxC4YAypLoirGTY",
-  authDomain: "ezen-test.firebaseapp.com",
-  projectId: "ezen-test",
-  storageBucket: "ezen-test.firebasestorage.app",
-  messagingSenderId: "980118669217",
-  measurementId: "G-FK9Z03NNFV",
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // apiKey: "AIzaSyDD6lh6FVHDXQE4K9ZvFxC4YAypLoirGTY",
+  // authDomain: "ezen-test.firebaseapp.com",
+  // projectId: "ezen-test",
+  // storageBucket: "ezen-test.firebasestorage.app",
+  // messagingSenderId: "980118669217",
+  // measurementId: "G-FK9Z03NNFV",
 
   // apiKey: "AIzaSyB2BzDQYK6mgABdzPmAA3nMTosBAA7LiPM",
   // authDomain: "hfashion-project.firebaseapp.com",
