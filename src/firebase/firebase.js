bd215fb2756b5ad75d8993ee0ca1f823bd0debe0
yuleehana <yuleehana@gmail.com>
2025-11-29 16:15:08 +0900

    test20

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,11 +5,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "hfashion-project.firebaseapp.com",
-  projectId: "hfashion-project",
-  storageBucket: "hfashion-project.firebasestorage.app",
-  messagingSenderId: "326422964641",
-  appId: "1:326422964641:web:1febdf63866201946cc3e0"
+ authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
